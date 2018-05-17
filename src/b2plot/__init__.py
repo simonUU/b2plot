3a830298273663b84c89bf6211bea9aa1466e273
Simon Wehle <simon.wehle@desy.de>
2018-05-17 12:17:09 +0200

    Fixes

--- src/b2plot/__init__.py
+++ src/b2plot/__init__.py
@@ -2,8 +2,8 @@ import pkg_resources
 
 __version__ = pkg_resources.get_distribution(__name__).version
 
-from _style import set_default_style, set_small_style
-from plot_wrappers import hist, errorhist
-from _manager import xaxis, nf
-from functions import draw_y_label, decorate
+from ._style import set_default_style, set_small_style
+from .plot_wrappers import hist, errorhist
+from ._manager import xaxis, nf
+from .functions import draw_y_label, decorate
 
